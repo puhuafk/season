3d8cacb6d6450e89f42d2e452bdf7403723307d7
kunfeng <1059609989@qq.com>
2020-09-14 16:06:32 +0800

    添加工具类

--- src/main/java/com/season/filter/NulsContext.java
+++ src/main/java/com/season/filter/NulsContext.java
@@ -1,14 +1,9 @@
 package com.season.filter;
 
 
-import io.nuls.core.tools.log.Log;
-import io.nuls.kernel.constant.KernelErrorCode;
-import io.nuls.kernel.exception.NulsRuntimeException;
-import io.nuls.kernel.lite.core.SpringLiteContext;
-import io.nuls.kernel.model.Block;
 
 import java.util.List;
- 
+
 public class NulsContext {
 
     public static volatile Integer WALLET_STATUS = 1;
@@ -34,18 +29,6 @@ public class NulsContext {
      */
     public static volatile Long MAIN_NET_VERSION_HEIGHT = 0L;
 
-    /**
-     * 最后一次升级，用于判断是否停止网络
-     *
-     * @return;
-     */
-    public static boolean isNetFinished(int delay) {
-        long bestHeight = NulsContext.getInstance().getBestHeight();
-        if (MAIN_NET_VERSION >= 4 && bestHeight - MAIN_NET_VERSION_HEIGHT >= delay) {
-            return true;
-        }
-        return false;
-    }
 
     /**
      * 切换序列化交易HASH方法的高度
@@ -72,16 +55,6 @@ public class NulsContext {
     public static byte CONTRACT_ADDRESS_TYPE = 2;
 
     public static String INITIAL_STATE_ROOT = "56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421";
-
-    /**
-     * cache the best block
-     */
-    private Block bestBlock;
-
-    /**
-     * cache the genesis block
-     */
-    private Block genesisBlock;
 
     /**
      * 同一个出块地址连续3轮发出两个相同高度，但不同hash的block，节点将会被红牌惩罚
@@ -114,163 +87,6 @@ public class NulsContext {
      * 是否需要强制升级
      */
     public static volatile boolean mastUpGrade = false;
-
-    /**
-     * 获取创世块
-     * get the block height is 0
-     *
-     * @return block
-     */
-    public Block getGenesisBlock() {
-        while (genesisBlock == null) {
-            try {
-                Thread.sleep(100L);
-            } catch (InterruptedException e) {
-            }
-        }
-        return genesisBlock;
-    }
-
-    public void setGenesisBlock(Block block) {
-        this.genesisBlock = block;
-    }
-
-    public Block getBestBlock() {
-        if (bestBlock == null) {
-            bestBlock = getGenesisBlock();
-        }
-        return bestBlock;
-    }
-
-    /**
-     * 获取本地最新高度
-     * Get the latest local height.
-     *
-     * @return long height
-     */
-    public long getBestHeight() {
-        if (bestBlock == null) {
-            bestBlock = getGenesisBlock();
-        }
-        return bestBlock.getHeader().getHeight();
-    }
-
-    private NulsContext() {
-    }
-
-    private static final NulsContext NC = new NulsContext();
-
-    /**
-     * get zhe only instance of NulsContext
-     *
-     * @return NulsContext
-     */
-    public static final NulsContext getInstance() {
-        return NC;
-    }
-
-
-    public void setBestBlock(Block bestBlock) {
-        if (bestBlock == null) {
-            throw new RuntimeException("best block set to null!");
-        }
-        this.bestBlock = bestBlock;
-//        Log.info("best height:"+bestBlock.getHeader().getHeight()+", hash:"+bestBlock.getHeader().getHash());
-    }
-
-    /**
-     * 根据服务类型获取实例
-     * Gets an instance based on the service type.
-     *
-     * @param tClass 服务类型
-     * @param <T>    泛型
-     * @return 实例对象
-     */
-    public static final <T> T getServiceBean(Class<T> tClass) {
-        try {
-
-            return SpringLiteContext.getBean(tClass);
-        } catch (Exception e) {
-            return getServiceBean(tClass, 0L);
-        }
-    }
-
-    private static <T> T getServiceBean(Class<T> tClass, long l) {
-        try {
-            Thread.sleep(200L);
-//            System.out.println("获取service失败！"+tClass);
-        } catch (InterruptedException e1) {
-            Log.error(e1);
-        }
-        try {
-            return SpringLiteContext.getBean(tClass);
-        } catch (NulsRuntimeException e) {
-            if (KernelErrorCode.DATA_ERROR.getCode().equals(e.getCode())) {
-                throw e;
-            }
-            if (l > 1200) {
-                Log.error(e);
-                return null;
-            }
-            return getServiceBean(tClass, l + 10L);
-        }
-    }
-
-    /**
-     * 获取网络最新高度，当缓存的网络最新高度为空或者小于本地高度时，返回本地最新高度
-     * To get the latest height of the network, return to the local latest height
-     * when the cached network's latest height is empty or less than the local height.
-     *
-     * @return long best height
-     */
-    public Long getNetBestBlockHeight() {
-        if (null != bestBlock && netBestBlockHeight < bestBlock.getHeader().getHeight()) {
-            return bestBlock.getHeader().getHeight();
-        }
-        if (null == netBestBlockHeight) {
-            return 0L;
-        }
-        return netBestBlockHeight;
-    }
-
-    /**
-     * 获取缓存的网络最新高度
-     * Gets the latest height of the cached network.
-     *
-     * @return Long
-     */
-    public Long getNetBestBlockHeightWithNull() {
-        return netBestBlockHeight;
-    }
-
-    public void setNetBestBlockHeight(Long netBestBlockHeight) {
-        this.netBestBlockHeight = netBestBlockHeight;
-    }
-
-    /**
-     * 根据服务类型获取该类型所有实例
-     * Gets all instances of this type according to the service type.
-     *
-     * @param tClass 服务类型
-     * @param <T>    泛型
-     * @return 实例列表
-     */
-    public static <T> List<T> getServiceBeanList(Class<T> tClass) {
-        try {
-            return SpringLiteContext.getBeanList(tClass);
-        } catch (Exception e) {
-            Log.error(e);
-        }
-        return null;
-    }
-
-    public short getDefaultChainId() {
-        if (null == defaultChainId) {
-            Log.error("Default chain id is null!");
-            throw new NulsRuntimeException(KernelErrorCode.DATA_ERROR);
-        }
-        return defaultChainId;
-    }
 
     public void setDefaultChainId(Short defaultChainId) {
         this.defaultChainId = defaultChainId;
